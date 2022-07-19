acc752b41923eec096cf092ddd4cfd1bcea13a07
Tushar <tusharchaurasia456@gmail.com>
2022-07-20 00:31:35 +0530

    adding field validation

--- entity/user.go
+++ entity/user.go
@@ -1,14 +1,16 @@
 package entity
 
 type User struct {
-	Name    string  `json:"name" binding:"required"`
+	Name    string  `json:"name" binding:"required,alphanum"`
 	Age     uint8   `json:"age" binding:"required,gte=1,lte=130"`
 	Email   string  `json:"email" binding:"required,email"`
+	Phone   string  `json:"phone" binding:"required,e164"`
+	DOB     string  `json:"dob" binding:"required,datetime=2006-01-02"`
 	Address Address `json:"address" binding:"required"`
 }
 
 type Address struct {
-	State   string `json:"state" binding:"required"`
-	City    string `json:"city" binding:"required"`
-	Pincode int    `json:"pincode" binding:"required"`
+	State   string `json:"state" binding:"required,alphanum"`
+	City    string `json:"city" binding:"required,alphanum"`
+	Pincode int    `json:"pincode" binding:"required,number"`
 }
--- helper/validate_error_message.go
+++ helper/validate_error_message.go
@@ -19,14 +19,21 @@ func customeMessageForTag(tag string) string {
 
 	switch tag {
 	case "required":
-		return "This field is required"
+		return "This field is required."
 	case "email":
-		return "Invalid email"
+		return "Invalid email."
 	case "lte":
-		return "less than 130"
+		return "less than 130."
 	case "gte":
-		return "greater than 1"
-
+		return "greater than 1."
+	case "datetime":
+		return "enter date in yyyy-mm-dd format."
+	case "e164":
+		return "phone number is invalid."
+	case "alphanum":
+		return "field should contains only alhabets and number"
+	case "number":
+		return "field should contains only number"
 	}
 	return ""
 }
