fdc57acdf575b7a8407efaac42696a6341519c43
Tushar <tusharchaurasia456@gmail.com>
2022-07-19 16:52:53 +0530

    added route for ping handler

--- server.go
+++ server.go
@@ -20,6 +20,8 @@ func main() {
 
 	server := gin.Default()
 	baspath := server.Group("/api/v1")
+	baspath.GET("/ping", pingHandler)
+
 	videoController.RegisterVideoRoutes(baspath)
 	userController.RegisterUserRoutes(baspath)
 	server.Run(":8080")
